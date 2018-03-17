5bc4907c48b010ff3ed1c43f05241a9c5f26feb9
Mamun Mokni <mamun.mokni@gmail.com>
2018-03-17 21:04:43 +0100

    Add babelrc and webpack.config files

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -3,18 +3,18 @@ import { render } from 'react-dom'
 const ce = React.createElement
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title))
+   return ce('div', null, ce('h1', { style: { color: props.color } }, props.title))
 }
 
 const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  )
+   return ce(
+      'div',
+      { id: 'my-first-component' },
+      ce(MyTitle, { title: 'Game of Thrones', color: 'FUCHSIA' }),
+      ce(MyTitle, { title: 'Breaking Bad', color: 'LIGHTCORAL' }),
+      ce(MyTitle, { title: 'Lost', color: 'MEDIUMSPRINGGREEN' }),
+      ce(MyTitle, { title: 'Silicon Valley', color: 'MEDIUMSLATEBLUE' })
+   )
 }
 
 render(ce(MyFirstComponent), document.getElementById('app'))
