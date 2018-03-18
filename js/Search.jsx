869ced1afb1622e140a63e3f5e19e3e470cf573c
Mamun Mokni <mamun.mokni@gmail.com>
2018-03-18 17:45:54 +0100

    add react-router Link in component

---
+++ js/Search.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+const Search = () => <h1>Hi lol this is a search</h1>
+export default Search
--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter, Route } from 'react-router-dom'
 import Landing from './landing'
+import Search from './Search'
 const app = document.querySelector('#app')
 
 const App = () => (
    <HashRouter>
       <div className="app">
          <Route exact path="/" component={Landing} />
+         <Route path="/search" component={Search} />
       </div>
    </HashRouter>
 )
