6a242d7d154fc689b9d7cf12554bbb8d7a8ede9b
Mamun Mokni <mamun.mokni@gmail.com>
2018-03-19 19:57:39 +0100

    Using the propTypes

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { shape, string } from 'prop-types'
 const ShowCard = props => (
    <div className="show-card">
       <img src={`/public/img/posters/${props.show.poster}`} alt={`${props.show.title} Show Card`} />
@@ -7,9 +7,21 @@ const ShowCard = props => (
          <h2>{props.show.title}</h2>
          <h3>{props.show.year}</h3>
          <p>{props.show.description}</p>
+         <p>{props.show.foo}</p>
 
       </div>
    </div>
 )
-
+ShowCard.defaultProps = {
+   foo: 'stuff'
+}
+ShowCard.propTypes = {
+   show: shape({
+      poster: string.isRequired,
+      title: string.isRequired,
+      year: string.isRequired,
+      description: string.isRequired
+   }).isRequired,
+   foo: string.isRequired
+}
 export default ShowCard
