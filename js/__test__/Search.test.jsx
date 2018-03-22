0f2ed220a150f41064a6049eb1d0e04368eaf4bd
Mamun Mokni <mamun.mokni@gmail.com>
2018-03-22 21:39:58 +0100

    Testing the number of ShowCards

--- js/__test__/Search.test.jsx
+++ js/__test__/Search.test.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Search from '../Search'
+import ShowCard from '../ShowCard'
+import preload from '../../data.json'
 
 // console.log(process.env.NODE_ENV)
 
-test('Search renders correctly', () => {
+xtest('Search renders correctly', () => {
    const component = shallow(<Search />)
    expect(component).toMatchSnapshot()
 })
+test('Search should render correct amount of shows ', () => {
+   const component = shallow(<Search />)
+   expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+test('Search hould render correct amount of shows based on search term', () => {})
